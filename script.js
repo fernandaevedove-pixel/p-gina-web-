dd9efad5a076a42feeb27c6ad5e2b74c6ffa9ed1
fernandaevedove-pixel <fernanda.evedove@escola.pr.gov.br>
2026-07-02 13:49:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const btn = document.getElementById("btn");
 
+const cores = [
+    "#f4f4f4",
+    "#FFD166",
+    "#06D6A0",
+    "#118AB2",
+    "#EF476F",
+    "#8338EC",
+    "#3A86FF"
+];
+
+let indice = 0;
+
+btn.addEventListener("click", () => {
+    indice = (indice + 1) % cores.length;
+    document.body.style.backgroundColor = cores[indice];
+});
